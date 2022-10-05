77ffa1adcf17f6ffd8db95200643b1951f61ba37
Long <15257546+lzztt@users.noreply.github.com>
2022-10-04 22:02:20 -0700

    Enable https on localhost dev environment

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # One Now
 
 ## Build Proto
+
 ```
 mkdir -p frontend/src/gen backend/gen
 
@@ -9,26 +10,48 @@ buf format proto -w
 buf generate
 ```
 
+## Create `localhost` certificate
+
+Install [mkcert](https://github.com/FiloSottile/mkcert), then create `localhost` certificate.
+```
+mkdir cert && cd cert
+
+mkcert localhost
+```
+
 ## Note files
+
 Note filenames have `<uuid>_<timestamp>.md` pattern.
 
 A note can have multiple versions. `uuid` identifies a note. `timestamp` identifies a version of a note.
 
+Commands to generate 10 dummy notes:
+```
+mkdir note && cd note
+
+for i in {1..10}; do
+    file=`uuidgen`_`date +%s`.md
+    echo -e "Title $i\n\nBody $i" > $file;
+done
+```
+
 
 ## Build Backend
+
 ```
 cd backend
 
-go run main.go -dir=../note -email=test@test.com
+go run main.go -dev -dir=../note -email=test@test.com
 go build
 ```
 
 ## Build Frontend
+
 ```
 cd frontend
 
 yarn install
 yarn test
-yarn start
+HTTPS=true SSL_CRT_FILE=../cert/localhost.pem SSL_KEY_FILE=../cert/localhost-key.pem yarn start
 yarn build
 ```
--- backend/main.go
+++ backend/main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/improbable-eng/grpc-web/go/grpcweb"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 
 	"one.now/backend/controller"
 	authv1 "one.now/backend/gen/proto/auth/v1"
@@ -17,12 +18,15 @@ import (
 )
 
 const (
-	dev = "http://localhost:3000"
+	devOrigin = "https://localhost:3000"
+	certFile  = "../cert/localhost.pem"
+	keyFile   = "../cert/localhost-key.pem"
 )
 
 var (
 	dir   = flag.String("dir", "", "The directory contains note files")
 	email = flag.String("email", "", "Allowed email to login")
+	dev   = flag.Bool("dev", false, "Run server in dev mode")
 )
 
 func main() {
@@ -33,14 +37,20 @@ func main() {
 	notev1.RegisterNoteServiceServer(gs, handler.NewNoteService(controller.NewNoteCtrler(*dir)))
 	authv1.RegisterAuthServiceServer(gs, handler.NewAuthService(controller.NewAuthCtrler(*email)))
 
-	wrappedServer := grpcweb.WrapServer(gs, grpcweb.WithOriginFunc(func(origin string) bool {
-		return origin == dev
-	}))
+	var originFunc func(string) bool
+	if *dev {
+		reflection.Register(gs)
+		originFunc = func(origin string) bool { return origin == devOrigin }
+	} else {
+		originFunc = func(origin string) bool { return false }
+	}
+
+	wrappedServer := grpcweb.WrapServer(gs, grpcweb.WithOriginFunc(originFunc))
 
 	http.Handle("/", handler.EnableSession(wrappedServer, repository.NewInMemorySession()))
 
-	log.Println("Serving on http://0.0.0.0:3080")
-	if err := http.ListenAndServe(":3080", nil); err != nil {
+	log.Println("Serving on https://localhost:3443")
+	if err := http.ListenAndServeTLS("localhost:3443", certFile, keyFile, nil); err != nil {
 		log.Fatal(err)
 	}
 }
